2b27c3974e0f405d896135aff461918a34c5095b
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-06-01 13:10:34 +0900

    Implement in Elixir

--- lesson/lib/lesson.ex
+++ lesson/lib/lesson.ex
@@ -15,4 +15,32 @@ defmodule Lesson do
   def hello do
     :world
   end
+
+  def myfunc do
+    "Hello, world!"
+  end
+
+  def sleep_rand(n) do
+    sleep_time = :rand.uniform(n)
+    :timer.sleep(sleep_time)
+    IO.puts "#{sleep_time}ミリ秒sleepしました"
+    sleep_time
+  end
+
+  def execute_sequentially(n) do
+    IO.puts "=== 開始: #{n}回rand_sleepを繰り返す ==="
+    result = Enum.map(1..n, fn(_) -> sleep_rand(1000) end)
+    IO.puts "=== 終了: #{n}回rand_sleepを繰り返す ==="
+    result
+  end
+
+  def execute_concurrently(n) do
+    IO.puts "=== 開始: #{n}回rand_sleepを繰り返す ==="
+    result = 1..n
+    |> Enum.map(fn(_) -> Task.async(Lesson, :sleep_rand, [1000]) end)
+    |> Enum.map(&Task.await/1)
+
+    IO.puts "=== 終了: #{n}回rand_sleepを繰り返す ==="
+    result
+  end
 end
